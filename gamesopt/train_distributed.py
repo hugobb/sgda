8a409659d95ff8c78fb3b9147511c80227b23e2a
Hugo Berard <berard.hugo@gmail.com>
2022-01-25 17:02:21 +0100

    Fixing bug

--- gamesopt/train_distributed.py
+++ gamesopt/train_distributed.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 from random import randrange
 from gamesopt.db import Record
 from .optimizer.base import DistributedOptimizer
@@ -19,8 +20,8 @@ class TrainDistributedConfig(TrainConfig):
     n_process: int = 2
     optimizer: OptimizerOptions = OptimizerOptions(optimizer_type=OptimizerType.QSGDA)
 
-def _train(rank, config: TrainDistributedConfig = TrainDistributedConfig(), record: Record = Record()) -> None:
-    setup(rank, config.n_process)
+def _train(rank: int, port: str, config: TrainDistributedConfig = TrainDistributedConfig(), record: Record = Record()) -> None:
+    setup(rank, config.n_process, port)
     
     print("Init...")
     game = load_game(config.game)
@@ -44,24 +45,27 @@ def _train(rank, config: TrainDistributedConfig = TrainDistributedConfig(), reco
             metrics["n_bits"].append(n_bits)
             record.save_metrics(metrics)
 
-def setup(rank: int, size: int, backend: str = 'gloo') -> None:
+def setup(rank: int, size: int, port: str, backend: str = 'gloo') -> None:
     os.environ['MASTER_ADDR'] = '127.0.0.1'
-    
-    # Tries to allocate a port until a port is available
-    while True:
-        port = str(random.randrange(1030, 49151))
-        print("Trying port %s" % port)
-        os.environ['MASTER_PORT'] = port
-        try:
-            dist.init_process_group(backend, rank=rank, world_size=size)
-            return
-        except Exception as e:
-            pass
+    os.environ['MASTER_PORT'] = port
+    dist.init_process_group(backend, rank=rank, world_size=size)
 
 
 def train(config: TrainDistributedConfig = TrainDistributedConfig(), record: Record = Record()) -> Record:
     record.save_config(config)
     torch.manual_seed(config.seed)
-    mp.spawn(_train, args=(config, record), nprocs=config.n_process, join=True)
+
+    # Tries to allocate a port until a port is available
+    while True:
+        port = str(random.randrange(1030, 49151))
+        print("Trying port %s" % port)
+        try:
+            mp.spawn(_train, args=(config, record, port), nprocs=config.n_process, join=True)
+            break
+        except RuntimeError as e:
+            print(e)
+        else:
+            raise
+    
     record.load_metrics()
     return record
