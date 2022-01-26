270a6bf925ae2865d6ead5bad49263465e7bf7cc
Hugo Berard <berard.hugo@gmail.com>
2022-01-26 15:43:16 +0100

    Updating matrix

--- gamesopt/games/quadratic_games.py
+++ gamesopt/games/quadratic_games.py
@@ -8,21 +8,15 @@ from typing import Optional, Union, Tuple
 from torch import linalg
 
 
-def make_random_matrix(num_players: int, num_samples: int, dim: int, mu: float = 0, L: float = 1., max_im: float = 1.) -> torch.Tensor:
-    if isinstance(L, float):
-        L_min = L
-        L_max = L
-    elif len(L) == 2:
-        L_min = L[0]
-        L_max = L[1]
-    else:
-        raise ValueError()
-
-    matrix = torch.randn(num_samples, num_players*dim, num_players*dim)
-    L, V = torch.linalg.eig(matrix)
-    L.real = abs(L.real)
-    matrix = (V @ torch.diag_embed(L) @ torch.linalg.inv(V)).real
-    #s = torch.linalg.eigvals(matrix)
+def make_random_matrix(num_samples: int, dim: int, mu: float = 0., ell: float = 1.) -> torch.Tensor:
+    matrix = torch.randn(num_samples, dim, dim)
+    eigs: torch.Tensor
+    eigs, V = torch.linalg.eig(matrix)
+    eigs.real = abs(eigs.real)
+    R_0 = ((1/eigs).real).min(-1, keepdim=True)[0]
+    eigs = eigs*R_0*ell
+    matrix = (V @ torch.diag_embed(eigs) @ torch.linalg.inv(V)).real
+    s = torch.linalg.eigvals(matrix)
     #print(s.real.max(dim=-1)[0].min(), s.real.max(dim=-1)[0].max(), s.real.min(), s.imag.max(), abs(s.imag).min())
     return matrix
 
@@ -33,9 +27,9 @@ class QuadraticGameConfig:
     num_players: int = 2
     bias: bool = True
     mu: float = 0.
-    L: Union[float, Tuple[float, float]] = 1.
-    max_im: float = 1.
+    ell: Union[float, Tuple[float, float]] = 1.
     importance_sampling: bool = False
+    matrix: Optional[torch.Tensor] = None
 
 
 class QuadraticGame(Game):
@@ -45,7 +39,9 @@ class QuadraticGame(Game):
         players = [torch.zeros(self._dim, requires_grad=True), torch.zeros(self._dim, requires_grad=True)]
         super().__init__(players, config.num_samples, rank, config.importance_sampling)
         
-        self.matrix = make_random_matrix(config.num_players, config.num_samples, config.dim, config.mu, config.L, config.max_im) 
+        self.matrix = config.matrix
+        if self.matrix is None:
+            self.matrix = make_random_matrix(config.num_samples, self.num_players*config.dim, config.mu, config.ell)
 
         self.bias = torch.zeros(2, config.num_samples, config.dim)
         if config.bias:
@@ -60,6 +56,7 @@ class QuadraticGame(Game):
         eigenvalues: torch.Tensor = linalg.eigvals(self.matrix)
         ell = 1 / ((1 / eigenvalues).real).min(-1)[0]
         self.p = ell / (ell.sum())
+        print(self.p)
 
     def reset(self) -> None:
         for i in range(self.num_players):
